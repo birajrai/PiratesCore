82ca3d770e82cc7c1f4010a70f21b2135aa17a20
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-07-06 14:24:11 +0545

    Create ConfigManager.java

---
+++ src/main/java/xyz/dapirates/managers/ConfigManager.java
@@ -0,0 +1,22 @@
+package xyz.dapirates.managers;
+
+import xyz.dapirates.Core;
+import xyz.dapirates.utils.OreMiningConfig;
+
+public class ConfigManager {
+    private final Core plugin;
+    private final OreMiningConfig oreMiningConfig;
+    private final WebhookManager webhookManager;
+
+    public ConfigManager(Core plugin, OreMiningConfig oreMiningConfig, WebhookManager webhookManager) {
+        this.plugin = plugin;
+        this.oreMiningConfig = oreMiningConfig;
+        this.webhookManager = webhookManager;
+    }
+
+    public void reloadAll() {
+        oreMiningConfig.reloadConfig();
+        webhookManager.reloadConfig();
+        plugin.getLogger().info("All configs reloaded.");
+    }
+} 
