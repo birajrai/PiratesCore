24ea6b82b82f2b8e34a73dcbd2a7926a6f5431cb
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-07-14 19:49:57 +0545

    Update ConfigManager.java

--- src/main/java/xyz/dapirates/manager/ConfigManager.java
+++ src/main/java/xyz/dapirates/manager/ConfigManager.java
@@ -2,6 +2,8 @@ package xyz.dapirates.manager;
 
 import xyz.dapirates.core.Core;
 import xyz.dapirates.utils.OreMiningConfig;
+import org.bukkit.configuration.file.YamlConfiguration;
+import java.io.File;
 
 /**
  * Central manager for reloading all plugin configurations and listeners.
@@ -10,11 +12,36 @@ public class ConfigManager {
     private final Core plugin;
     private final OreMiningConfig oreMiningConfig;
     private final WebhookManager webhookManager;
+    private YamlConfiguration databaseConfig;
+    private String dbHost;
+    private String dbDatabase;
+    private String dbUser;
+    private String dbPassword;
 
     public ConfigManager(Core plugin, OreMiningConfig oreMiningConfig, WebhookManager webhookManager) {
         this.plugin = plugin;
         this.oreMiningConfig = oreMiningConfig;
         this.webhookManager = webhookManager;
+    }
+
+    public void loadDatabaseConfig() {
+        try {
+            File dbFile = new File(plugin.getDataFolder(), "Database.yml");
+            if (!dbFile.exists()) {
+                plugin.saveResource("Database.yml", false);
+            }
+            databaseConfig = YamlConfiguration.loadConfiguration(dbFile);
+            dbHost = databaseConfig.getString("mysql.host", "localhost");
+            dbDatabase = databaseConfig.getString("mysql.database", "piratescore");
+            dbUser = databaseConfig.getString("mysql.user", "root");
+            dbPassword = databaseConfig.getString("mysql.password", "password");
+        } catch (Exception e) {
+            plugin.getLogger().severe("Failed to load Database.yml: " + e.getMessage());
+            dbHost = "localhost";
+            dbDatabase = "piratescore";
+            dbUser = "root";
+            dbPassword = "";
+        }
     }
 
     /**
@@ -26,5 +53,19 @@ public class ConfigManager {
         plugin.getFeatureManager().getChatFilterListener().reload();
         plugin.getFeatureManager().reloadSettings();
         plugin.getLogger().info("All configs reloaded.");
+    }
+
+    // Add methods to get MySQL config from Database.yml
+    public String getDatabaseUrl() {
+        if (dbHost == null || dbDatabase == null) loadDatabaseConfig();
+        return "jdbc:mysql://" + dbHost + ":3306/" + dbDatabase + "?useSSL=false&autoReconnect=true";
+    }
+    public String getDatabaseUser() {
+        if (dbUser == null) loadDatabaseConfig();
+        return dbUser;
+    }
+    public String getDatabasePassword() {
+        if (dbPassword == null) loadDatabaseConfig();
+        return dbPassword;
     }
 }
