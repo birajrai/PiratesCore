151e8c243d5c6ef5e5782f2a22a963725edfed88
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-07-14 18:40:36 +0545

    Update ConfigManager.java

--- src/main/java/xyz/dapirates/manager/ConfigManager.java
+++ src/main/java/xyz/dapirates/manager/ConfigManager.java
@@ -3,6 +3,9 @@ package xyz.dapirates.manager;
 import xyz.dapirates.core.Core;
 import xyz.dapirates.utils.OreMiningConfig;
 
+/**
+ * Central manager for reloading all plugin configurations and listeners.
+ */
 public class ConfigManager {
     private final Core plugin;
     private final OreMiningConfig oreMiningConfig;
@@ -14,10 +17,13 @@ public class ConfigManager {
         this.webhookManager = webhookManager;
     }
 
+    /**
+     * Reloads all configs and listeners in the plugin.
+     */
     public void reloadAll() {
         oreMiningConfig.reloadConfig();
         webhookManager.reloadConfig();
-        plugin.getChatFilterListener().reload();
+        plugin.getFeatureManager().getChatFilterListener().reload();
         plugin.getFeatureManager().reloadSettings();
         plugin.getLogger().info("All configs reloaded.");
     }
