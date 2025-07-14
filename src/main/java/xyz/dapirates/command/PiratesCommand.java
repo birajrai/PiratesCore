48ad1e50cd39611b65b574df9c28c91690cfd084
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2025-07-14 18:40:19 +0545

    Update PiratesCommand.java

--- src/main/java/xyz/dapirates/command/PiratesCommand.java
+++ src/main/java/xyz/dapirates/command/PiratesCommand.java
@@ -8,19 +8,18 @@ import xyz.dapirates.core.Core;
 public class PiratesCommand implements CommandExecutor {
     private final Core plugin;
 
-    public PiratesCommand(Core plugin) {
+    public PiratesCommand(final Core plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
         if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
             if (!sender.hasPermission("pirates.reload")) {
                 sender.sendMessage("§cYou don't have permission to use this command!");
                 return true;
             }
             plugin.getConfigManager().reloadAll();
-            plugin.getChatFilterListener().reload();
             sender.sendMessage("§a[PiratesAddons] All configs reloaded!");
             return true;
         }
